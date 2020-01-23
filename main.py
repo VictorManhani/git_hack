26a937211c62e7d4262e921e4e75c7526f3f5233
VictorManhani <@moDeus2018>
2020-01-23 22:21:12 +0000

    2020-01-23 22:21:12.907178+00:00

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 # https://www.programiz.com/python-programming/json
+# https://gitpython.readthedocs.io/en/stable/
+# https://www.youtube.com/watch?v=2q--gA97caM
+# __author__ in javascript: Akshay Saini
+# __author__ in python: Victor Manhani
 
 import json
 import git
@@ -7,33 +11,42 @@ import random
 import pytz
 import datetime
 
+# initialize the git class
 repo = git.Git()
+# get the files in the current repository
 files = [arquivo for arquivo in os.listdir('.')]
+# file name
 FILE_PATH = 'data.json'
 
 # n is the quantity of the commits
 def make_commit(n):
+    # recursive resolve
     if n == 0:
         return repo.push()
-
-    # x is the week and y is the day.
-    x = random.randint(0,49)
-    y = random.randint(0,6)
+    # days to be subtracted
     days = datetime.timedelta(days = 364)
-    x = datetime.timedelta(weeks = x)
-    y = datetime.timedelta(days = y)
+    # x is the week to be add between the 0 and 49 week.
+    x = datetime.timedelta(weeks = random.randint(0,49))
+    # y is the day of the week to be add between 0 and 6.
+    y = datetime.timedelta(days = random.randint(0,6))
+    # get the now time 
     today = datetime.datetime.now(pytz.utc)
+    # sum and parser to string the date
     DATE = str(today - days + x + y)
-    
-    data = {"date": str(DATE)}
-    print(data['date'])
-    
+    # create a dict for save in json file
+    data = {"date": DATE}
+    print(data)
+    # write the changes in json file
     with open(FILE_PATH, 'w') as json_file:
         json.dump(data, json_file)
-
+    # add files in directory to stage area
     repo.add(files)
+    # commit the staged files
     repo.commit(m = DATE, date = DATE)
+    # increment minus 1 in the counter 
     n -= 1
+    # recursive call with the n decreased
     make_commit(n)
 
+# call the function with the n times it will be called
 make_commit(1)
